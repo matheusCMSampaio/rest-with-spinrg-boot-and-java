72f999ca16ffa5bfe88dbe67dd2cc824c68e9179
matheuscmsampaio <matheuschagas415@gmail.com>
2024-12-09 17:08:01 -0300

    First steps in Java Udemy

---
+++ src/main/java/br/com/Java/rest_with_spinrg_boot_and_java/Greeting.java
@@ -0,0 +1,20 @@
+package br.com.Java.rest_with_spinrg_boot_and_java;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/test/java/br/com/Java/rest_with_spinrg_boot_and_java/StartUpTests.java
@@ -0,0 +1,13 @@
+package br.com.Java.rest_with_spinrg_boot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
